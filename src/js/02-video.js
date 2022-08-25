59a06675293a9a50219c7a5a30a314ffb7550658
vladakalyta <vladakalyta@gmail.com>
2022-08-25 17:45:57 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', onPlay);
+
+function onPlay({ seconds }) {
+  localStorage.setItem("videoplayer-current-time", JSON.stringify(seconds))
+}
+
+player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")))
 
