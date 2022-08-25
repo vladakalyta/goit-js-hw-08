882140ac8a0f163387b7dc4383ee62eeecefb19b
vladakalyta <vladakalyta@gmail.com>
2022-08-25 17:53:33 +0300

    task 2 (throttle)

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,10 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', onPlay);
+player.on('timeupdate', throttle(onPlay, 1000) );
 
 function onPlay({ seconds }) {
   localStorage.setItem("videoplayer-current-time", JSON.stringify(seconds))
